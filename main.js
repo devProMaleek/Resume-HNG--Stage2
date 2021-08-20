448c844c7fbdaf30b501c3fe39c432d27745ec74
devProMaleek <milikiadbay@gmail.com>
2021-08-20 23:55:19 +0100

    Completed the print name

---
+++ main.js
@@ -0,0 +1,26 @@
+// TYPING ANIMATION SCRIPT 
+
+// Name Type
+let typed = new Typed(".name-typing", {
+    strings: ["Abdulmalik Adebayo"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
+
+// Skill Type
+let typedSkill = new Typed(".skill-typing", {
+    strings: ["Jnr Frontend Developer", "UI/UX Designer", "Freelancer", "Jnr Data Scientist"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
+
+// Nick Type
+let typedNick = new Typed(".nick-typing", {
+    strings: ["devProMaleek"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
+
